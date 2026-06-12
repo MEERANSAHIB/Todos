f56b543a5c11c6a2a27708e0a75e52dc49ce9410
MEERANSAHIB <meeransahib005@gmail.com>
2026-06-12 11:55:10 +0530

    Added a post API endpoint to add new user and save them in the database with hashed password

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,41 @@
-from fastapi import APIRouter
-router=APIRouter()
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
+from database import SessionLocal
+from models import Users
+
+router=APIRouter()
+bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
 @router.get("/auth")
 async def auth():
     return {"message":"user is authenticated"}
+class createUserRequest(BaseModel):
+    email:str=Field()
+    user_name:str
+    first_name:str
+    last_name:str
+    hashed_password:str
+    role:str
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+db_dependency=Annotated[Session,Depends(get_db)]
+@router.post("/auth/new_user_request")
+async def createuser(db:db_dependency,new_user:createUserRequest):
+    user_model=Users(
+        email=new_user.email,
+        user_name=new_user.user_name,
+        first_name=new_user.first_name,
+        last_name=new_user.last_name,
+        hashed_password=bcrypt_context.hash(new_user.hashed_password),
+        role=new_user.role,
+        isactive=True
+    )
+    db.add(user_model)
+    db.commit()
